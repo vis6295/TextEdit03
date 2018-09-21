824bc3abd0b030c476dd1c352e440e68ceba6148
sboev <sboev@moretravel.ru>
2018-09-21 12:14:03 +0500

    Обновление 1

---
+++ TextEdit.Lib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextEdit.Lib
+{
+    public class Class1
+    {
+        public override string ToString()
+        {
+            Console.WriteLine("Class1");
+            return "Class1";
+        }
+    }
+}
